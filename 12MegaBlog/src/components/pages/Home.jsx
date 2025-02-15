455813d70719180ae36530129ae9814ba5121b62
Sujal Gupta <sujalgupta3797@gmail.com>
2025-02-15 11:36:54 +0530

    project ongoing..

---
+++ 12MegaBlog/src/components/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react'
+import appwriteService from '../appwrite/config'
+import {Container, PostCard} from '../components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            if(posts) {
+                setPosts(posts.documents)
+            }
+        })
+    })
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
